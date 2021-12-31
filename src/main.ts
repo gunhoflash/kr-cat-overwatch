dca2464a083ed41093fcdef89394aad66f6d2e04
dewy.i <dewy.i@kakaocorp.com>
2021-12-31 21:36:35 +0900

    Init project

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+  await app.listen(process.env.HTTP_PORT);
+  console.log(`Application is running on: ${process.env.HTTP_PORT}`);
+}
+bootstrap();
